6fb07c5cbcfc994163f155c4ca93dbb68aa2879e
michael walker <miwalker@atlassian.com>
2021-08-22 00:15:01 -0500

    added global permission set to initial group creation

--- resources/mirror_operations.py
+++ resources/mirror_operations.py
@@ -48,25 +48,52 @@ class ActionOnItems:
         group_counter = 0
         group_memberships = 0
         
-        for group in groups_to_migrate:
-            if not CA.create_group(cloud, group):
-                print(f'WARN: Failed to mirror group "{group}" to your cloud instance for unknown reason.')
+        for group_name in groups_to_migrate:
+            if not CA.create_group(cloud, group_name):
+                print(f'WARN: Failed to mirror group "{group_name}" to your cloud instance for unknown reason.')
                 continue
+            if not ActionOnItems.add_group_global_perms(cloud, group_name, global_groups):
+                print(f'WARN: Failed to apply global permissions to {group_name} within your cloud instance.')
             group_migration = {'total_users': [], 'migrated_users': []}
             group_counter += 1
-            for member in SA.get_group_members(server, group):
+            for member in SA.get_group_members(server, group_name):
                 group_migration['total_users'].append(member.emailAddress)
-                if CA.add_member_to_group(cloud, group, member):
+                if CA.add_member_to_group(cloud, group_name, member):
                     group_migration['migrated_users'].append(member.emailAddress)
                     group_memberships += 1
 
             if len(group_migration['total_users']) == len(group_migration['migrated_users']):
-                print(f'INFO: Successfully migrated all {len(group_migration["total_users"])} users in group: {group}')
+                print(f'INFO: Successfully migrated all {len(group_migration["total_users"])} users in group: {group_name}')
             else:
-                print(f"WARN: Successfully migrated {len(group_migration['migrated_users'])} of {len(group_migration['total_users'])} members of {group}. Failed to mirror the following users to the group's membership:")
+                print(f"WARN: Successfully migrated {len(group_migration['migrated_users'])} of {len(group_migration['total_users'])} members of {group_name}. Failed to mirror the following users to the group's membership:")
                 print(', '.join([user_email for user_email in group_migration['total_users'] if user_email not in group_migration['migrated_users']]))
 
         print(f'INFO: Mirrored {group_counter} groups with {group_memberships} group membership assignments')
+
+    @staticmethod
+    def add_group_global_perms(cloud: CloudInstance, group_name: str, global_groups: list[Group]) -> bool:
+        try:
+            group = [group for group in global_groups if group.name == group_name][0]
+        except IndexError:
+            # the group_name doesn't have to be found within global_groups so simply skip if this is the case
+            return True
+        if group.permission == "LICENSED_USER":
+            # do nothing as this is already implied by existing in the workspace
+            return True
+        elif group.permission == "PROJECT_CREATE":
+            privilege = "None" # No default read/write/admin on any existing content
+            account_privilege = "collaborator" # Can create new content though
+        elif group.permission == "ADMIN":
+            privilege = "Admin" # Admin on all existing content
+            account_privilege = "collaborator" # Can create new content
+        elif group.permission == "SYS_ADMIN":
+            privilege = "Admin" # Admin on all existing content
+            account_privilege = "admin" # Admin on workspace itself
+
+        if CA.set_group_global_permissions(cloud, group.name, privilege, account_privilege):
+            return True
+
+        return False
 
     @staticmethod
     def max_permission(project_permission: str, repo_permission: str, repo_default: str) -> str:
