4490a3e03d60ad03d51749dd41610ee5a9180749
michael walker <miwalker@atlassian.com>
2021-08-30 14:31:46 -0500

    added comment explaining each field

--- env_template.py
+++ env_template.py
@@ -1,7 +1,7 @@
-server_username = ""
-server_password = ""
-server_url = ""
+server_username = ""  # Your admin level username
+server_password = ""  # Your admin level password or personal access token https://confluence.atlassian.com/bitbucketserver/personal-access-tokens-939515499.html
+server_url = ""  # Your Server/DC's base URL https://confluence.atlassian.com/bitbucketserver077/specifying-the-base-url-for-bitbucket-server-1026551743.html
 
-cloud_username = ""
-cloud_password = ""
-cloud_workspace = ""
+cloud_username = ""  # Your admin level username
+cloud_password = ""  # Your admin level password or app password https://support.atlassian.com/bitbucket-cloud/docs/app-passwords/
+cloud_workspace = ""  # Your workspace name/ID (just the name, not the URL) https://support.atlassian.com/bitbucket-cloud/docs/what-is-a-workspace/
